9047cd7e86a6e9bd87d7d6e7170146cf4e9fee21
FINKI-ALST <62511297+FINKI-ALST@users.noreply.github.com>
2020-06-10 14:56:38 +0200

    Project upload

---
+++ SI_lab2_171037/src/main/java/SILab2.java
@@ -0,0 +1,46 @@
+class User {
+    String username;
+    String password;
+    String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
+public class SILab2 {
+
+    public static boolean function (User user, String allUsers) { //1
+        if (user!=null) { //2
+            if (user.getUsername()!=null && user.getEmail()!=null && !allUsers.contains(user.getUsername())) { //3
+                boolean atChar = false, dotChar = false; //4
+                for (int i=0;i<user.getEmail().length();i++) { //5
+                    if (user.getEmail().charAt(i)=='@') //6
+                        atChar = true; //7
+                    if (atChar && user.getEmail().charAt(i)=='.') { //8
+                        dotChar = true; //9
+                    } //10
+                } //11
+                if (atChar && dotChar) { //12
+                    return true; //13
+                } //14
+            } //15
+        } //16
+        return false; //17
+    } //18
+}
+
---
+++ SI_lab2_171037/src/test/java/SILab2Test.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SILab2ATest {
+    private final User user(String username, String password, String email){
+        return user(username,password,email);
+    }
+
+    @Test
+    void TestEveryPath (){
+        // 1 2 16 17 18
+        assertEquals(false, SILab2.function(null,"a,b,c"));
+        // 1 2 3 15 16 17 18
+        assertEquals(false, SILab2.function(user(null,null,"a"),"a,b,c"));
+        // 1 2 3 4 5.1 (5.2 6 7 8 9 10 11 5.3 5.2) 12 13 14 18 True
+        assertEquals(true, SILab2.function(user("alek","pass","a@gmail.com"),"a,b,c,alek"));
+        // 1 2 3 4 5.1 5.2 11 12 14 18 False - cant happen
+        // 1 2 3 4 5.1 (5.2 6 7 8 10 11 5.3 5.2) 12 14 18 False mixed (bez 9)
+        assertEquals(false, SILab2.function(user("alek","pass","a@gmailcom"),"a,b,c,alek"));
+        // 1 2 3 4 5.1 (5.2 6 8 9 10 11 5.3 5.2) 12 14 18 False mixed (bez 7)
+        assertEquals(false, SILab2.function(user("alek","pass","agmail.com"),"a,b,c,alek"));
+
+        // 1 2 3 4 5.1 (5.2 6 8 10 11 5.3 5.2) 12 14 18 False mixed
+        assertEquals(false, SILab2.function(user("alek","pass","agmailcom"),"a,b,c,alek"));
+
+    }
+
+    @Test
+    void multipleConditionsTest() {
+        //if (user.getUsername()!=null && user.getEmail()!=null && !allUsers.contains(user.getUsername())) { //3
+        //F && X && X
+        //T && F && X
+        //T && T && F
+
+        assertEquals(false, SILab2.function(user(null,"pass","a@gmail.com"),"a,b,c"));
+        assertEquals(false, SILab2.function(user("alek","pass",null),"a,b,c"));
+        assertEquals(false, SILab2.function(user("alek","pass","a@gmail.com"),"a,b,c"));
+
+        //if (atChar && user.getEmail().charAt(i)=='.') { //8
+        //F && X
+        //T && F
+
+        assertEquals(false, SILab2.function(user("alek","pass","agmail.com"),"a,b,c,alek"));
+        assertEquals(false, SILab2.function(user("alek","pass","a@gmailcom"),"a,b,c,alek"));
+
+        //if (atChar && dotChar) { //12
+        //F && X
+        //T && F
+
+        assertEquals(false, SILab2.function(user("alek","pass","agmail.com"),"a,b,c,alek"));
+        assertEquals(false, SILab2.function(user("alek","pass","a@gmailcom"),"a,b,c,alek"));
+    }
+}
